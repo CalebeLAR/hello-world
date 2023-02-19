aa0f3ea5044cd2fdf254c74adcf89451577f0f55
CalebeLAR <ca6ele@gmail.com>
2023-02-19 17:35:58 -0400

    tipos de métodos HTTP

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
 const express = require('express');
+
 const app = express();
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+// app.get('/', (req, res) => res.status(200).send('<h1>Olá Mundo!</h1>'));
+// app.get('/', (req, res) => res.status(200).redirect('https://www.betrybe.com/'));
+// app.get('/', (req, res) => res.status(200).render('./index.html'));
+// app.get('/', (req, res) => res.status(200).end());
+
+
+
+
 module.exports = app;
