f9f3a2a5efcc141da9b03a939e0276cdd3c2865a
CalebeLAR <ca6ele@gmail.com>
2023-02-19 22:13:03 -0400

    metodo get

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
+const { query } = require('express');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+// app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 // app.get('/', (req, res) => res.status(200).send('<h1>Olá Mundo!</h1>'));
 // app.get('/', (req, res) => res.status(200).redirect('https://www.betrybe.com/'));
 // app.get('/', (req, res) => res.status(200).render('./index.html'));
 // app.get('/', (req, res) => res.status(200).end());
+
+// Query strings
+app.get('/search', (req, res) => {
+  console.log(req.query.name);
+  console.log(req.query.age);
+  res.status(200).send('<h1>Search ok</h1>');
+})
+
+// Parãmetros de rotas
+app.post('/people', (req, res) => {
+  res.status(200).send('People ok!');
+})
+
+// Corpo de requisição
+app.post('/tasks', (req, res) => {
+  res.status(200).send('Tasks ok!');
+})
 
 
 
